efdfde5a1f3f483c59f7491c01283ea0c64b69e3
alex <alex@equipmoz.org>
2022-11-03 15:24:15 +0200

    add js to handle UI blocks

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContents = document.querySelectorAll('.tab-content-item');
 
+function selectItem(e) {
+  //add the border and show
+  removeBorder();
+  removeShow();
+  this.classList.add('tab-border');
+
+  //get content
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add('show');
+}
+
+function removeBorder() {
+  tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+  tabContents.forEach(item => item.classList.remove('show'))
+}
+tabItems.forEach(item => item.addEventListener('click', selectItem));
